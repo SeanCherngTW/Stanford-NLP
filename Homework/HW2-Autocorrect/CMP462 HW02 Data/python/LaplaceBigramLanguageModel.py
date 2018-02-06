1bda6a7da8f89428f2370c8508bc4ba30be135a8
Sean Cherng <seancherng.tw@gmail.com>
2018-02-06 21:21:21 +0800

    update 3 language models

--- Homework/HW2-Autocorrect/CMP462 HW02 Data/python/LaplaceBigramLanguageModel.py
+++ Homework/HW2-Autocorrect/CMP462 HW02 Data/python/LaplaceBigramLanguageModel.py
@@ -1,20 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import math
+import collections
+
+
 class LaplaceBigramLanguageModel:
 
-  def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
-    self.train(corpus)
+    def __init__(self, corpus):
+        """Initialize your data structures in the constructor."""
+        self.laplaceUnigramCounts = collections.defaultdict(lambda: 0)
+        self.laplaceBigramCounts = collections.defaultdict(lambda: 0)
+        self.V = 0
+        self.train(corpus)
 
-  def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
-        Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    pass
+    def train(self, corpus):
+        """ Takes a corpus and trains your language model.
+            Compute any counts or other corpus statistics in this function.
+        """
+        for sentence in corpus.corpus:
+            first_token = sentence.data[0].word
+            for token in sentence.data[1:]:
+                second_token = token.word
+                self.laplaceUnigramCounts[first_token] += 1
+                self.laplaceBigramCounts[first_token, second_token] += 1
+                first_token = second_token
 
-  def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
-        sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    return 0.0
+        # add-one smoothing
+        self.V += len(self.laplaceBigramCounts.keys())
+
+    def score(self, sentence):
+        """ Takes a list of strings as argument and returns the log-probability of the
+            sentence using your language model. Use whatever data you computed in train() here.
+
+            log(P(wn∣wn−1)) = log(C(wn−1,wn)+1 / C(wn−1)+V)
+            log(P(wn∣wn−1)) = log(C(wn−1,wn)+1) - log(C(wn−1)+V)
+            V is the number of distinct bigram
+        """
+        score = 0.0
+        first_token = sentence[0]
+        for second_token in sentence[1:]:
+            unigram_count = self.laplaceUnigramCounts[second_token]
+            bigram_count = self.laplaceBigramCounts[first_token, second_token]
+            bigram_uni_count = self.laplaceUnigramCounts[first_token]
+
+            score += math.log(bigram_count + 1)  # log(C(wn−1,wn)+1)
+            score -= math.log(bigram_uni_count + self.V)  # -log(C(wn−1)+V)
+
+            first_token = second_token
+        return score
--- Homework/HW2-Autocorrect/CMP462 HW02 Data/python/StupidBackoffLanguageModel.py
+++ Homework/HW2-Autocorrect/CMP462 HW02 Data/python/StupidBackoffLanguageModel.py
@@ -1,20 +1,65 @@
+import math
+import collections
+
+
 class StupidBackoffLanguageModel:
 
-  def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
-    self.train(corpus)
+    def __init__(self, corpus):
+        """Initialize your data structures in the constructor."""
+        self.unigramCounts = collections.defaultdict(lambda: 0)
+        self.bigramCounts = collections.defaultdict(lambda: 0)
+        self.trigramCounts = collections.defaultdict(lambda: 0)
+        self.unigram_total = 0
+        self.train(corpus)
 
-  def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
-        Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    pass
+    def train(self, corpus):
+        """ Takes a corpus and trains your language model.
+            Compute any counts or other corpus statistics in this function.
+        """
+        for sentence in corpus.corpus:
+            first_token = sentence.data[0].word
+            second_token = sentence.data[1].word
 
-  def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
-        sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    return 0.0
+            for token in sentence.data[2:]:
+                third_token = token.word
+                self.unigram_total += 1
+                self.unigramCounts[first_token] += 1
+                self.bigramCounts[first_token, second_token] += 1
+                self.trigramCounts[first_token, second_token, third_token] += 1
+                first_token, second_token = second_token, third_token
+
+        # add-one smoothing for unigram
+        self.unigram_total += len(self.unigramCounts.keys())
+
+    def score(self, sentence):
+        """ Takes a list of strings as argument and returns the log-probability of the
+            sentence using your language model. Use whatever data you computed in train() here.
+        """
+        score = 0.0
+        length = len(sentence)
+        first_token = sentence[0]
+        second_token = sentence[1]
+
+        for third_token in sentence[2:]:
+            unigram_count = self.unigramCounts[third_token]
+            bigram_count = self.bigramCounts[second_token, third_token]
+            bigram_uni_count = self.unigramCounts[second_token]
+            trigram_count = self.trigramCounts[first_token, second_token, third_token]
+            trigram_bi_count = self.bigramCounts[(first_token, second_token)]
+
+            if trigram_count > 0:
+                score += math.log(trigram_count)
+                score -= math.log(trigram_bi_count)
+
+            elif bigram_count > 0:
+                # score += math.log(0.4)
+                score += math.log(bigram_count)
+                score -= math.log(bigram_uni_count)
+
+            else:
+                # score += math.log(0.16)
+                score += math.log(unigram_count + 1)
+                score -= math.log(self.unigram_total)
+
+            first_token, second_token = second_token, third_token
+        return score
