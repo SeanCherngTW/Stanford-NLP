ebf81e172b40501fa137846aade5f13618e01bdf
Sean Cherng <seancherng.tw@gmail.com>
2018-02-14 15:05:49 +0800

    Update HW5 F1=0.8305

---
+++ Homework/HW5-Name-Entity-Recognition/CMP462 HW05 Data/python/Datum.py
@@ -0,0 +1,7 @@
+class Datum:
+    def __init__(self, word, label):
+        self.word = word
+        self.label = label
+        self.guessLabel = ''
+        self.previousLabel = ''
+        self.features = []
